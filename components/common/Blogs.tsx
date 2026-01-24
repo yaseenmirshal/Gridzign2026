e6c473edcfdd67a578dffa043e634fc657b861a4
gridzign <gridzign@email.com>
2026-01-24 11:43:44 +0530

    images updated

--- components/common/Blogs.tsx
+++ components/common/Blogs.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import { blogs1 } from "@/data/blogs.json";
 import RevealText from "../animation/RevealText";
-import BackgroundParallax from "../animation/BackgroundParallax";
 import AnimatedButton from "../animation/AnimatedButton";
+
 const defaultDesc = `Inspiring ideas, creative insights, and the latest in design and tech. Fueling innovation for your digital journey.`;
 
 interface BlogsProps {
@@ -31,11 +30,13 @@ export default function Blogs({
                     </RevealText>
                   </div>
                 </div>
+
                 <div className="col-12 col-xl-4 mxd-grid-item no-margin">
                   <div className="mxd-section-title__hrdescr">
                     <p className="anim-uni-in-up">{desc}</p>
                   </div>
                 </div>
+
                 <div className="col-12 col-xl-3 mxd-grid-item no-margin">
                   <div className="mxd-section-title__hrcontrols anim-uni-in-up">
                     <AnimatedButton
@@ -52,56 +53,119 @@ export default function Blogs({
           </div>
         </div>
         {/* Block - Section Title End */}
+
         {/* Block - Blog Preview Cards Start */}
         <div className="mxd-block">
           <div className="mxd-blog-preview">
             <div className="container-fluid p-0">
               <div className="row g-0">
-                {blogs1.map((item, idx) => (
-                  <div
-                    key={idx}
-                    className="col-12 col-xl-4 mxd-blog-preview__item mxd-grid-item animate-card-3"
-                  >
-                    <Link
-                      className="mxd-blog-preview__media"
-                      href={`/blog-article`}
-                    >
-                      <BackgroundParallax
-                        className={`mxd-blog-preview__image ${item.imageClass} parallax-img-small`}
+                {/* Blog Card 1 */}
+                <div className="col-12 col-xl-4 mxd-blog-preview__item mxd-grid-item animate-card-3">
+                  <Link className="mxd-blog-preview__media" href={`/blog-article`}>
+                    <div className="mxd-blog-preview__image relative overflow-hidden">
+                      <Image
+                        src="/img/blog/frontend.png"
+                        alt="Blog 1"
+                        fill
+                        className="object-cover"
                       />
-                      <div className="mxd-preview-hover">
-                        <i className="mxd-preview-hover__icon">
-                          <Image
-                            alt="Eye Icon"
-                            src="/img/icons/icon-eye.svg"
-                            width={38}
-                            height={21}
-                          />
-                        </i>
-                      </div>
-                      <div className="mxd-blog-preview__tags">
-                        {item.tags.map((tag, tIdx) => (
-                          <span
-                            key={tIdx}
-                            className="tag tag-default tag-permanent"
-                          >
-                            {tag}
-                          </span>
-                        ))}
-                      </div>
-                    </Link>
-
-                    <div className="mxd-blog-preview__data">
-                      <Link className="anim-uni-in-up" href={`/blog-article`}>
-                        {item.title.before ?? ""}{" "}
-                        {item.title.highlight ? (
-                          <span>{item.title.highlight}</span>
-                        ) : null}{" "}
-                        {item.title.after ?? ""}
-                      </Link>
                     </div>
+
+                    <div className="mxd-preview-hover">
+                      <i className="mxd-preview-hover__icon">
+                        <Image
+                          alt="Eye Icon"
+                          src="/img/icons/icon-eye.svg"
+                          width={38}
+                          height={21}
+                        />
+                      </i>
+                    </div>
+
+                    <div className="mxd-blog-preview__tags">
+                      <span className="tag tag-default tag-permanent">Design</span>
+                      <span className="tag tag-default tag-permanent">UI/UX</span>
+                    </div>
+                  </Link>
+
+                  <div className="mxd-blog-preview__data">
+                    <Link className="anim-uni-in-up" href={`/blog-article`}>
+                      How to build <span>Modern UI</span> in 2026
+                    </Link>
                   </div>
-                ))}
+                </div>
+
+                {/* Blog Card 2 */}
+                <div className="col-12 col-xl-4 mxd-blog-preview__item mxd-grid-item animate-card-3">
+                  <Link className="mxd-blog-preview__media" href={`/blog-article`}>
+                    <div className="mxd-blog-preview__image relative overflow-hidden">
+                      <Image
+                        src="https://cdn.searchenginejournal.com/wp-content/uploads/2023/03/untitled-design-43-6405cb01dcfc7-sej.png"
+                        alt="Blog 2"
+                        fill
+                        className="object-cover"
+                      />
+                    </div>
+
+                    <div className="mxd-preview-hover">
+                      <i className="mxd-preview-hover__icon">
+                        <Image
+                          alt="Eye Icon"
+                          src="/img/icons/icon-eye.svg"
+                          width={38}
+                          height={21}
+                        />
+                      </i>
+                    </div>
+
+                    <div className="mxd-blog-preview__tags">
+                      <span className="tag tag-default tag-permanent">Tech</span>
+                      <span className="tag tag-default tag-permanent">Next.js</span>
+                    </div>
+                  </Link>
+
+                  <div className="mxd-blog-preview__data">
+                    <Link className="anim-uni-in-up" href={`/blog-article`}>
+                      Boost your <span>Website Speed</span> like a Pro
+                    </Link>
+                  </div>
+                </div>
+
+                {/* Blog Card 3 */}
+                <div className="col-12 col-xl-4 mxd-blog-preview__item mxd-grid-item animate-card-3">
+                  <Link className="mxd-blog-preview__media" href={`/blog-article`}>
+                    <div className="mxd-blog-preview__image relative overflow-hidden">
+                      <Image
+                        src="https://png.pngtree.com/thumb_back/fh260/background/20260112/pngtree-comprehensive-corporate-brand-identity-mockup-set-displayed-on-an-office-desk-image_20997254.webp"
+                        alt="Blog 3"
+                        fill
+                        className="object-cover"
+                      />
+                    </div>
+
+                    <div className="mxd-preview-hover">
+                      <i className="mxd-preview-hover__icon">
+                        <Image
+                          alt="Eye Icon"
+                          src="/img/icons/icon-eye.svg"
+                          width={38}
+                          height={21}
+                        />
+                      </i>
+                    </div>
+
+                    <div className="mxd-blog-preview__tags">
+                      <span className="tag tag-default tag-permanent">Branding</span>
+                      <span className="tag tag-default tag-permanent">Creative</span>
+                    </div>
+                  </Link>
+
+                  <div className="mxd-blog-preview__data">
+                    <Link className="anim-uni-in-up" href={`/blog-article`}>
+                      Create a <span>Strong Brand</span> identity fast
+                    </Link>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
